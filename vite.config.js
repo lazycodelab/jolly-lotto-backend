7093fd802c9b09b76ffc08388ab26075416237fb
adictonator <adityabhaskarsharma@gmail.com>
2022-10-21 20:12:35 +0530

    added inertia and react config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+	plugins: [
+		laravel({
+			input: 'resources/js/app.jsx',
+			ssr: 'resources/js/ssr.jsx',
+			refresh: true,
+			valetTls: 'jolly-lotto.test',
+		}),
+		react(),
+	],
+	ssr: {
+		noExternal: ['@inertiajs/server'],
+	},
+})
