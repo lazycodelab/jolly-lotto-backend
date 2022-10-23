9bec9ac8fac115b4b17a6236fadad1d9ca381ea6
adictonator <adityabhaskarsharma@gmail.com>
2022-10-23 17:59:58 +0530

    added some fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
-    ],
+	content: [
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php',
+		'./resources/js/**/*.jsx',
+	],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+	theme: {
+		extend: {
+			fontFamily: {
+				heebo: ['Heebo', ...defaultTheme.fontFamily.sans],
+				lato: ['Lato', ...defaultTheme.fontFamily.sans],
+				'open-sans': ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
 
-    plugins: [require('@tailwindcss/forms')],
-};
+	plugins: [require('@tailwindcss/forms')],
+}
