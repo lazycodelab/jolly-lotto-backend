a773e089025be086d2eabd4e9824b02cab2ed5bb
adictonator <adityabhaskarsharma@gmail.com>
2022-10-26 18:59:16 +0530

    some footer ui update

--- resources/js/Shared/Footer.jsx
+++ resources/js/Shared/Footer.jsx
@@ -1,16 +1,100 @@
 export default () => {
 	return (
 		<footer className="bg-cyan-50 py-20 border-t border-gray-200">
-			<div className="container mx-auto">
-				<div className="grid">
+			<div className="container mx-auto max-w-6xl">
+				<div className="grid grid-cols-3 grid-rows-2 gap-y-8 gap-x-10">
 					<div>
-						<h4 className="text-xl">Learn About Lotto Express</h4>
+						<h4 className="text-xl text-cyan-500 font-semibold">
+							Learn About Lotto Express
+						</h4>
 						<ul>
 							<li>
 								<a href="#">About Lotto Express</a>
 							</li>
 						</ul>
 					</div>
+					<div>
+						<h4 className="text-xl text-cyan-500 font-semibold">
+							Learn About Lotto Express
+						</h4>
+						<ul>
+							<li>
+								<a href="#">About Lotto Express</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h4 className="text-xl text-cyan-500 font-semibold">
+							Learn About Lotto Express
+						</h4>
+						<ul>
+							<li>
+								<a href="#">About Lotto Express</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h4 className="text-xl text-cyan-500 font-semibold">
+							Learn About Lotto Express
+						</h4>
+						<ul>
+							<li>
+								<a href="#">About Lotto Express</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h4 className="text-xl text-cyan-500 font-semibold">
+							Learn About Lotto Express
+						</h4>
+						<ul>
+							<li>
+								<a href="#">About Lotto Express</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h4 className="text-xl text-cyan-500 font-semibold">
+							Learn About Lotto Express
+						</h4>
+						<ul>
+							<li>
+								<a href="#">About Lotto Express</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div className="divide-x divide-cyan-900 space-x-5 text-center mt-16">
+					<a href="#" className="font-base text-cyan-900">
+						Responsible Gaming
+					</a>
+					<a href="#" className="pl-5 font-base text-cyan-900">
+						Responsible Gaming
+					</a>
+					<a href="#" className="pl-5 font-base text-cyan-900">
+						Responsible Gaming
+					</a>
+				</div>
+
+				<div className="mt-10 flex justify-center gap-10 items-center">
+					<img
+						className="max-w-[180px]"
+						src="images/ST_Footer_GCRC.png"
+					/>
+					<img className="max-w-[180px]" src="images/lock-icon.png" />
+					<img
+						className="max-w-[180px]"
+						src="images/ST_Footer_18.png"
+					/>
+					<img
+						className="max-w-[180px]"
+						src="images/ST_Footer_GA.svg"
+					/>
+					<img
+						className="max-w-[180px]"
+						src="images/ST_Footer_GC.svg"
+					/>
 				</div>
 			</div>
 		</footer>
