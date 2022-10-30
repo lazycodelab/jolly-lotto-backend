0adb36eb6bd34110fe980dffe82b68418e5039b8
adictonator <adityabhaskarsharma@gmail.com>
2022-10-31 00:20:13 +0530

    added schema for price and lotteries table

---
+++ database/migrations/2022_10_29_121320_create_lotteries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lotteries', function (Blueprint $table) {
+            $table->id();
+			$table->foreignId('product_id');
+			$table->string('country_code');
+			$table->string('currency_code');
+			$table->json('cut_offs');
+			$table->json('draw_dates');
+			$table->json('balls');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lotteries');
+    }
+};
