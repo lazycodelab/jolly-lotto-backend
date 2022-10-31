a151564033e3c7dc3c09cae82ab7ce98916b8e91
adictonator <adityabhaskarsharma@gmail.com>
2022-10-31 18:01:08 +0530

    trying to redeploy

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,6 +5,7 @@ export default function Guest({ children }) {
 	return (
 		<div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
 			<div>
+				logo
 				<Link href="/">logo</Link>
 			</div>
 
