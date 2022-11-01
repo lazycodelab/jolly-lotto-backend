872c4067ceb1d6c40eb007ed213a235b521a0050
adictonator <adityabhaskarsharma@gmail.com>
2022-11-01 23:05:56 +0530

    added lotteries routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+Route::get('/lotteries', [ProductController::class, 'index']);
+Route::get('/lotteries/{product}', [ProductController::class, 'show']);
 
 require __DIR__.'/auth.php';
