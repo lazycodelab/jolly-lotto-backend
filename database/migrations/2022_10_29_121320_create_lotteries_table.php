cf599eecb3a09d8979eaa27886053011f7aedad0
adictonator <adityabhaskarsharma@gmail.com>
2022-11-09 15:32:47 +0530

    updated column data type to work with returned val

--- database/migrations/2022_10_29_121320_create_lotteries_table.php
+++ database/migrations/2022_10_29_121320_create_lotteries_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('lotteries', function (Blueprint $table) {
             $table->id();
 			$table->foreignId('product_id')->unique();
-			$table->dateTime('start_date')->nullable();
+			$table->string('start_date')->nullable();
 			$table->string('country_code');
 			$table->string('currency_code');
 			$table->json('cut_offs');
