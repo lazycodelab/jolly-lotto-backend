544677518aee2aa80d0abd782259fddb7f244164
adictonator <adityabhaskarsharma@gmail.com>
2023-04-09 19:46:58 +0530

    fixed indentations and formatting some other minor fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,31 +8,31 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-		$schedule->call(function() {
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		$schedule->call(function () {
 			(new ProductController)->binChilling();
 		})->hourly();
-		$schedule->call(function() {
+		$schedule->call(function () {
 			(new ProductController)->binChillingResults();
 		})->hourly();
-    }
+	}
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		$this->load(__DIR__ . '/Commands');
 
-        require base_path('routes/console.php');
-    }
+		require base_path('routes/console.php');
+	}
 }
--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -13,47 +13,47 @@ use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'email'    => ['required', 'string', 'email'],
+			'password' => ['required', 'string'],
+		];
+	}
 
-    /**
-     * Attempt to authenticate the request's credentials.
-     *
-     * @return void
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function authenticate()
-    {
-        $this->ensureIsNotRateLimited();
+	/**
+	 * Attempt to authenticate the request's credentials.
+	 *
+	 * @return void
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	public function authenticate()
+	{
+		$this->ensureIsNotRateLimited();
 
 		$response = Http::lotto()->post('/auth/signin', [
-			'email' => $this->email,
+			'email'    => $this->email,
 			'password' => $this->password
 		]);
 
 		// @todo Move this logic to a separate file.
-		if ( $response->status() === 404 ) {
+		if ($response->status() === 404) {
 			// The token has expired.
 			$token = Http::withoutVerifying()
 				->withOptions(
@@ -63,7 +63,7 @@ class LoginRequest extends FormRequest
 				)->post(
 					'http://gateway.cloudandahalf.com/crow/api/auth/token',
 					[
-						'clientId' => env('API_KEY'),
+						'clientId'       => env('API_KEY'),
 						'clientSecurity' => env('API_SECRET'),
 					]
 				);
@@ -73,12 +73,12 @@ class LoginRequest extends FormRequest
 			Cache::put('api_token', $token->body(), now()->addMinutes(1440));
 
 			$response = Http::lotto()->post('/auth/signin', [
-				'email' => $this->email,
+				'email'    => $this->email,
 				'password' => $this->password
 			]);
 
 			// Again 404? Edge case.
-			if ( $response->status() === 404 ) {
+			if ($response->status() === 404) {
 				throw ValidationException::withMessages([
 					'email' => trans('This account is not available. Server error.'),
 				]);
@@ -87,50 +87,81 @@ class LoginRequest extends FormRequest
 
 			$user = $response->json();
 
-			if ( $user['statusCode'] === 200) {
-				session(['user' => $user]);
+			if ($user['statusCode'] === 200) {
+				// Now fetch user details.
+				//dd($user);
 
-				RateLimiter::clear($this->throttleKey());
+				$userDetails = Http::lotto()->get('/JL/accounts/' . $user['userId'] . '/details');
+				if ($userDetails->status() !== 200) {
+					$token = Http::withoutVerifying()
+						->withOptions(
+							[
+								'verify' => false,
+							]
+						)->post(
+							'http://gateway.cloudandahalf.com/crow/api/auth/token',
+							[
+								'clientId'       => env('API_KEY'),
+								'clientSecurity' => env('API_SECRET'),
+							]
+						);
+
+					// store cache for a day.
+					// @todo: maybe fix this logic.
+					Cache::put('api_token', $token->body(), now()->addMinutes(1440));
+
+					$userDetails = Http::lotto()->get('/JL/accounts/' . $user['userId'] . '/details');
+				}
+
+				$userDetails = $userDetails->json();
+
+				if ($userDetails && $userDetails['isSuccess'] === true) {
+					$userData = array_merge($user, $userDetails);
+
+					session(['user' => $userData]);
+
+					RateLimiter::clear($this->throttleKey());
+				}
 			} else {
 				throw ValidationException::withMessages([
 					'email' => trans($user['message']),
 				]);
 			}
 		}
-    }
+	}
 
-    /**
-     * Ensure the login request is not rate limited.
-     *
-     * @return void
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function ensureIsNotRateLimited()
-    {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
-            return;
-        }
+	/**
+	 * Ensure the login request is not rate limited.
+	 *
+	 * @return void
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	public function ensureIsNotRateLimited()
+	{
+		if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+			return;
+		}
 
-        event(new Lockout($this));
+		event(new Lockout($this));
 
-        $seconds = RateLimiter::availableIn($this->throttleKey());
+		$seconds = RateLimiter::availableIn($this->throttleKey());
 
-        throw ValidationException::withMessages([
-            'email' => trans('auth.throttle', [
-                'seconds' => $seconds,
-                'minutes' => ceil($seconds / 60),
-            ]),
-        ]);
-    }
+		throw ValidationException::withMessages([
+			'email' => trans('auth.throttle', [
+				'seconds' => $seconds,
+				'minutes' => ceil($seconds / 60),
+			]),
+		]);
+	}
 
-    /**
-     * Get the rate limiting throttle key for the request.
-     *
-     * @return string
-     */
-    public function throttleKey()
-    {
-        return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
-    }
+	/**
+	 * Get the rate limiting throttle key for the request.
+	 *
+	 * @return string
+	 */
+	public function throttleKey()
+	{
+		return Str::transliterate(Str::lower($this->input('email')) . '|' . $this->ip());
+	}
 }
