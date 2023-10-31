b9255c4473f7abe79a3ace20658fb2a2e911b082
Nisarg <kavinisarg1@gmail.com>
2023-10-31 16:28:41 +0530

    Added Resend Confirmation mail functionality

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -36,12 +36,28 @@ class VerifyEmailController extends Controller
 
 	public function validateHash(Request $request)
 	{
-		$hash = explode('?p=',$request->hash);
+		$hash = explode('?p=',$request->fullUrl());
 		$response = Http::lotto()->get("/auth/confirmemailaddress/". $hash[1]);
 		if ($response->status() === 200) {
 			return redirect()->intended(
 				config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=1'
 			);
+		}
+	}
+
+	public function resendEmail(Request $request)
+	{
+		$userID = session()->get('user.userId');
+		$response = Http::lotto()->get('/auth/resendconfirmemailaddress/' . $userID);
+		if ($response->status() === 200) {
+			$data = $response->json();
+			if($data['isSuccessStatusCode'] === true || isset($data['isSuccessStatusCode'])) {
+				return response()->json(['status' => 'success', 'message' => 'Email sent successfully.'], 200);
+			} else {
+				return response()->json(['status' => 'error', 'message' => 'Server Error'], 500);
+			}
+		} else {
+			return response()->json(['status' => 'error', 'message' => 'Something went wrong. Please try again later.'], 500);
 		}
 	}
 }
