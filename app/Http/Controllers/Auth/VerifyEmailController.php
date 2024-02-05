fe4975d1687faf2f7e14ced0fd1610f204a8ddb6
Nisarg <kavinisarg1@gmail.com>
2024-02-05 17:56:47 +0530

    Test 1

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -36,17 +36,18 @@ class VerifyEmailController extends Controller
 
 	public function validateHash(Request $request)
 	{
-		$hash = explode('?p=',$request->fullUrl());
-		$response = Http::lotto()->get("/auth/confirmemailaddress/". $hash[1]);
-		if ($response->status() === 200) {
-			$this->updateUserData('verifyEmail');
-			return $this->redirectAfterVerification();
-		}
+		$hash = explode('?p=', $request->fullUrl());
+		$response = Http::lotto()->get("/auth/confirmemailaddress/" . $hash[1]);
+		// if ($response->status() === 200) {
+		// 	$this->updateUserData('verifyEmail');
+		// 	return $this->redirectAfterVerification();
+		// }
 	}
 
-	public function redirectAfterVerification() {
+	public function redirectAfterVerification()
+	{
 		$products = session('products');
-		if($products === null || empty($products)) {
+		if ($products === null || empty($products)) {
 			return redirect()->intended(
 				config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=1'
 			);
@@ -76,17 +77,18 @@ class VerifyEmailController extends Controller
 		}
 	}
 
-	public function updateUserData($updateType) {
+	public function updateUserData($updateType)
+	{
 		$userID = session()->get('user.userId');
 		$currentSessionDetails = session()->get('user', null);
 		$response = Http::lotto()->get("/JL/accounts/$userID/details");
 		if ($response->status() === 401 || $response->status() === 403) {
 			$this->getAuthToken();
-		} else if($response->status() === 404) {
-			return response()->json(['status' => 'error', 'message' => 'No response from server. - ' . $response->status()] , 200);
-		} else if($response->status() === 200) {
+		} else if ($response->status() === 404) {
+			return response()->json(['status' => 'error', 'message' => 'No response from server. - ' . $response->status()], 200);
+		} else if ($response->status() === 200) {
 			$updateData = $response->json();
-			if($updateType === 'removeFunds') {
+			if ($updateType === 'removeFunds') {
 				$currentSessionDetails['wallet']['available'] = $updateData['wallet']['available'];
 				$currentSessionDetails['wallet']['withDrawal'] = $updateData['wallet']['withDrawal'];
 				session(['user' => $currentSessionDetails]);
@@ -96,7 +98,7 @@ class VerifyEmailController extends Controller
 			}
 			return $currentSessionDetails;
 		} else {
-			return response()->json(['status' => 'error', 'message' => 'Something went wrong, Please try again later!'] , 200);
+			return response()->json(['status' => 'error', 'message' => 'Something went wrong, Please try again later!'], 200);
 		}
 	}
 
@@ -106,7 +108,7 @@ class VerifyEmailController extends Controller
 		$response = Http::lotto()->get('/auth/resendconfirmemailaddress/' . $userID);
 		if ($response->status() === 200) {
 			$data = $response->json();
-			if($data['isSuccessStatusCode'] === true || isset($data['isSuccessStatusCode'])) {
+			if ($data['isSuccessStatusCode'] === true || isset($data['isSuccessStatusCode'])) {
 				return response()->json(['status' => 'success', 'message' => 'Email sent successfully.'], 200);
 			} else {
 				return response()->json(['status' => 'error', 'message' => 'Server Error'], 500);
